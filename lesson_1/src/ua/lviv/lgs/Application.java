1913a20f3beaa1b36aecf92d837d16e2995c991a
LogosItAcademy <voloshynmykolay@gmail.com>
2019-02-20 21:41:24 +0200

    added first project to gitHub

---
+++ lesson_1/src/ua/lviv/lgs/Application.java
@@ -0,0 +1,44 @@
+package ua.lviv.lgs;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+//		int f = 1;
+//		int g = 2;
+//		int h = 3;
+//		
+//		f += 5;
+//		g *= 4;
+//		h += f * g;
+//		h %= 6;
+//		
+//		System.out.println("f = " + f);
+//		System.out.println("g = " + g);
+//		System.out.println("h = " + h);
+///////////////////////////////////////////////////////////////		
+//		int a = 2;
+//		
+//		int r = a > 0 ? -3 : 2;
+//		System.out.println("r = " + r);
+////////////////////////////////////////////////////////////		
+//		boolean j = true;
+//		boolean k = false;
+//		boolean l = j | k;  // true
+//		boolean m = j & k;  // false
+//		boolean n = (!j & k) | (j & !k);  // true
+//		boolean o = !j;  // false
+//		
+//		System.out.println("j = " + j);
+//		System.out.println("k = " + k);
+//		System.out.println("l = " + l);
+//		System.out.println("m = " + m);
+//		System.out.println("n = " + n);
+//		System.out.println("o = " + o);
+////////////////////////////////////////////////////////////////////
+		// int, byte, short, long, double, float, char, boolean
+		
+		
+	}
+	
+}
